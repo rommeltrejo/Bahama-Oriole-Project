f184a71a04b48d3e342f08ba1647f5082b04312e
rommeltrejo <rommeltrejo@gmail.com>
2017-04-16 15:29:00 -0400

    heyy

--- public/lib/main.js
+++ public/lib/main.js
@@ -53,14 +53,7 @@ function ResearchForm() {
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
-ResearchForm.prototype.initFirebase = function() {
-  // Shortcuts to Firebase SDK features.
-  this.auth = firebase.auth();
-  this.database = firebase.database();
-  this.storage = firebase.storage();
-  // Initiates Firebase auth and listen to auth state changes.
-  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
-};
+ResearchForm.prototype.initFirebase = 3;
 
 // Loads chat messages history and listens for upcoming ones.
 ResearchForm.prototype.loadMessages = function() {
