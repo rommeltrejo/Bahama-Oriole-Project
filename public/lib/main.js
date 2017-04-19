7d46b41238f789c03d8bbebc972968efecaf647b
rommeltrejo <rommeltrejo@gmail.com>
2017-04-19 15:19:24 -0400

    sign in/out button fixed.. pic is comming back soon :)

--- public/lib/main.js
+++ public/lib/main.js
@@ -15,51 +15,116 @@
  */
 'use strict';
 
+
+
 // Initializes ResearchForm.
 function ResearchForm() {
 
+    this.submitButton = document.getElementsByClassName("btn btn-primary");
     this.userPic = document.getElementById('user-pic');
-  this.username = document.getElementById('user-name');
-  this.signInButton = document.getElementById('sign-in');
-  this.signOutButton = document.getElementById('sign-out');
-  
+    this.username = document.getElementById('user-name');
+    this.signInButton = document.getElementById('sign-in');
+    this.signOutButton = document.getElementById('sign-out');
+
     
-    /*
+
     this.checkSetup();
+    this.initFirebase();
 
-  // Shortcuts to DOM Elements.
-  this.messageList = document.getElementById('messages');
-  this.messageForm = document.getElementById('message-form');
-  this.messageInput = document.getElementById('message');
-  this.submitButton = document.getElementById('submit');
-  this.submitImageButton = document.getElementById('submitImage');
-  this.imageForm = document.getElementById('image-form');
-  this.mediaCapture = document.getElementById('mediaCapture');
-  this.userPic = document.getElementById('user-pic');
-  this.username = document.getElementById('user-name');
-  this.signInButton = document.getElementById('sign-in');
-  this.signOutButton = document.getElementById('sign-out');
-  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+    this.signOutButton.addEventListener('click', this.signOut.bind(this));
+    this.signInButton.addEventListener('click', this.signIn.bind(this));
 
-  // Saves message on form submit.
-  this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
-  this.signOutButton.addEventListener('click', this.signOut.bind(this));
-  this.signInButton.addEventListener('click', this.signIn.bind(this));
 
-  // Toggle for the button.
-  var buttonTogglingHandler = this.toggleButton.bind(this);
-  this.messageInput.addEventListener('keyup', buttonTogglingHandler);
-  this.messageInput.addEventListener('change', buttonTogglingHandler);
+    /*
+ // Shortcuts to DOM Elements.
 
-    
-  // Events for image upload.
-  this.submitImageButton.addEventListener('click', function() {
-    this.mediaCapture.click();
-  }.bind(this));*/
 
-//  this.mediaCapture.addEventListener('change', //this.saveImageMessage.bind(this));
-  this.initFirebase();
+ // Saves message on form submit.
+
+     this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+ // Toggle for the button.
+ var buttonTogglingHandler = this.toggleButton.bind(this);
+ this.messageInput.addEventListener('keyup', buttonTogglingHandler);
+ this.messageInput.addEventListener('change', buttonTogglingHandler);
+
+ ;*/
+
 }
+
+//useful
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+ResearchForm.prototype.checkSetup = function() {
+    if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+        window.alert('You have not configured and imported the Firebase SDK. ' +
+            'Make sure you go through the codelab setup instructions.');
+    } else if (config.storageBucket === '') {
+        window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+            'actually a Firebase bug that occurs rarely. ' +
+            'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+            'and make sure the storageBucket attribute is not empty. ' +
+            'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+            'displayed there.');
+    }else
+    {
+        console.log("firebase connection established successfully.")
+    }
+
+};
+
+// Signs-in Friendly Chat.
+ResearchForm.prototype.signIn = function() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    this.auth.signInWithPopup(provider);
+};
+
+// Signs-out of Friendly Chat.
+ResearchForm.prototype.signOut = function() {
+    // Sign out of Firebase.
+    this.auth.signOut();
+};
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+ResearchForm.prototype.onAuthStateChanged = function(user) {
+    if (user) { // User is signed in!
+        // Get profile pic and user's name from the Firebase user object.
+        var profilePicUrl = user.photoURL;
+        var username = user.displayName;
+
+        // Set the user's profile pic and name.
+        this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
+        this.username.textContent = username;
+
+        // Show user's profile and sign-out button.
+        this.username.removeAttribute('hidden');
+        this.userPic.removeAttribute('hidden');
+        this.signOutButton.removeAttribute('hidden');
+
+        // Hide sign-in button.
+        this.signInButton.setAttribute('hidden', 'true');
+
+        // We load currently existing chant messages.
+        // this.loadMessages();
+
+        // We save the Firebase Messaging Device token and enable notifications.
+        this.saveMessagingDeviceToken();
+    } else { // User is signed out!
+        // Hide user's profile and sign-out button.
+        this.username.setAttribute('hidden', 'true');
+        this.userPic.setAttribute('hidden', 'true');
+        this.signOutButton.setAttribute('hidden', 'true');
+
+        // Show sign-in button.
+        this.signInButton.removeAttribute('hidden');
+    }
+};
+
+
+
+
+//I have no idea if used
+
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
 ResearchForm.prototype.initFirebase = function() {
@@ -189,53 +254,6 @@ ResearchForm.prototype.saveImageMessage = function(event) {
   }
 };
 
-// Signs-in Friendly Chat.
-ResearchForm.prototype.signIn = function() {
-  // Sign in Firebase using popup auth and Google as the identity provider.
-  var provider = new firebase.auth.GoogleAuthProvider();
-  this.auth.signInWithPopup(provider);
-};
-
-// Signs-out of Friendly Chat.
-ResearchForm.prototype.signOut = function() {
-  // Sign out of Firebase.
-  this.auth.signOut();
-};
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-ResearchForm.prototype.onAuthStateChanged = function(user) {
-  if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL;
-    var username = user.displayName;
-
-    // Set the user's profile pic and name.
-    this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
-    this.username.textContent = username;
-
-    // Show user's profile and sign-out button.
-    this.username.removeAttribute('hidden');
-    this.userPic.removeAttribute('hidden');
-    this.signOutButton.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    this.signInButton.setAttribute('hidden', 'true');
-
-    // We load currently existing chant messages.
-   // this.loadMessages();
-
-    // We save the Firebase Messaging Device token and enable notifications.
-    this.saveMessagingDeviceToken();
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    this.username.setAttribute('hidden', 'true');
-    this.userPic.setAttribute('hidden', 'true');
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-  }
-};
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
 ResearchForm.prototype.checkSignedInWithMessage = function() {
@@ -416,24 +434,15 @@ ResearchForm.prototype.toggleButton = function() {
   }
 };
 
-// Checks that the Firebase SDK has been correctly setup and configured.
-ResearchForm.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
-  } else if (config.storageBucket === '') {
-    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
-        'actually a Firebase bug that occurs rarely. ' +
-        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
-        'and make sure the storageBucket attribute is not empty. ' +
-        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
-        'displayed there.');
-  }
-};
 
 window.onload = function() {
-  window.Researchform = new ResearchForm();
+
 };
 
+$( document ).ready(
+  function () {
+      window.Researchform = new ResearchForm();
+  }
+);
 
 
